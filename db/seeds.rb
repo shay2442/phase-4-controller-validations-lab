57c5011604e8b79abff346771e92dec638931fb8
shaywacker <shay2442@yahoo.com>
2022-02-11 13:44:59 -0600

    commit tree 6fae13cfe431e2344b24c96d83de505512a7b424

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@ Post.create!(
   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat.",
   category: "Non-Fiction"
 )
+
+Author.create!(name:"Shay Allison", email:"shay2442@yahoo.com")
+
+Post.create(title:"Learning to Code", content:"Learning to code can be challenging. It comes with all the ups and downs. Sometimes you are flying high becuase you figured out how to make a method or action work, and other times you are thinking I am never going to understand all of this, and I cannot figure out how to make this work", category:"Non-Fiction")
+
